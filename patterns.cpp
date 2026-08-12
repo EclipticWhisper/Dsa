5f7a3e69d8eb67f345ee29cdbb9d96d4fc38b64b
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-12 23:12:26 +0500

    Learned about square increasing number pattern

--- patterns.cpp
+++ patterns.cpp
@@ -2,14 +2,29 @@
 using namespace std;
 int main()
 {
+    // Pattern for printing Characters:
+    //  int n = 4;
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      char ch = 'A';
+    //      for (int j = 0; j < n; j++)
+    //      {
+    //          cout << " " << ch;
+    //          ch = ch + 1; // implicit type conversion from char to int and then back to char
+    //      }
+    //      cout << endl;
+    //  }
+
+    // Pattern for printing numbers like 1 2 3 4 5 6 7 ....
     int n = 4;
+    int num = 1;
     for (int i = 0; i < n; i++)
     {
-        char ch = 'A';
+
         for (int j = 0; j < n; j++)
         {
-            cout << " " << ch;
-            ch = ch + 1; // implicit type conversion from char to int and then back to char
+            cout << " " << num;
+            num++;
         }
         cout << endl;
     }
