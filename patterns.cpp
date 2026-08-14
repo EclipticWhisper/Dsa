27c021babc4878de7e5235a2e95d907b0199bac7
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-14 09:54:06 +0500

    Pyramid

--- patterns.cpp
+++ patterns.cpp
@@ -61,25 +61,25 @@ int main()
     // }
 
     // pyramid patterns
-    int n = 4;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            cout << " ";
-        }
-        // int num = 1;
-        for (int j = 1; j <= i + 1; j++)
-        {
-            cout << j;
-            // num++;
-        }
-        for (int j = i; j >= 1; j--)
-        {
-            cout << j;
-        }
-        cout << endl;
-    }
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     // int num = 1;
+    //     for (int j = 1; j <= i + 1; j++)
+    //     {
+    //         cout << j;
+    //         // num++;
+    //     }
+    //     for (int j = i; j >= 1; j--)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
 
     return 0;
 }
