d97fb9c4929a4efb4cb32e362801b45a172a09f2
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-15 23:08:27 +0500

    IsPRime

--- functions.cpp
+++ functions.cpp
@@ -25,30 +25,59 @@ using namespace std;
 //     return digitSum;
 // }
 
-//finding bionomial coefficient nCr
-int factorial(int n)
+// finding bionomial coefficient nCr
+//  int factorial(int n)
+//  {
+//      int fact = 1;
+//      for (int i = 1; i <= n; i++)
+//      {
+//          fact *= i;
+//      }
+//      return fact;
+//  }
+
+// int nCr(int n, int r)
+// {
+//     int factN = factorial(n);
+//     int factR = factorial(r);
+//     int factNR = factorial(n - r);
+
+//     return factN / (factR * factNR);
+// }
+
+// prime or not
+#include <iostream>
+using namespace std;
+
+int isPrime(int n)
 {
-    int fact = 1;
-    for (int i = 1; i <= n; i++)
+    // Numbers less than or equal to 1 are not prime
+    if (n <= 1)
+        return 0;
+
+    // Check numbers from 2 up to n-1
+    for (int i = 2; i < n; i++)
     {
-        fact *= i;
+        if (n % i == 0)
+        {
+            // Found a factor! It's definitely NOT prime. Exit immediately.
+            return 0;
+        }
     }
-    return fact;
-}
 
-int nCr(int n, int r)
-{
-    int factN = factorial(n);
-    int factR = factorial(r);
-    int factNR = factorial(n - r);
-
-    return factN / (factR * factNR);
+    // Checked all numbers and found no factors. It IS prime!
+    return 1;
 }
 
 int main()
 {
-    int n = 8, r = 2;
-
-    cout << nCr(n, r) << endl;
+    if (isPrime(5) == 1)
+    {
+        cout << "5 is a Prime Number" << endl;
+    }
+    else
+    {
+        cout << "5 is Not a Prime Number" << endl;
+    }
     return 0;
 }
