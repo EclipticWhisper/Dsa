a97ed87d0658bebe6d6d47f0e8d09d7c5582f99e
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-26 22:28:32 +0500

    index of array

--- Arrays.cpp
+++ Arrays.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = { 2, 3, 4, 5,1};
     int size = 5;
     int smallest = INT_MAX; // Initialize smallest to the maximum integer value
+    int index;
     for (int i = 0; i < size; i++)
     {
         if (arr[i] < smallest)
         {
             smallest = arr[i];
         }
+        if (arr[i] == smallest)
+        {
+            index = i;
+        }
     }
 
-    cout << "The smallest element in the array is: " << smallest << endl;
+    cout << "The smallest element in the array is: " << smallest << " at index " << index << endl;
 
     return 0;
 }
