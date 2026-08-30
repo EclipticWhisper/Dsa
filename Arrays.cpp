95bf28e8be9d0a70b95a8c3e40797d2bc9c5ba41
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-31 00:23:51 +0500

    Linear Search Algorithim

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,14 +2,26 @@
 #include <climits>
 using namespace std;
 
-void changeArr(int arr[], int size)
+// void changeArr(int arr[], int size)
+// {
+//     cout << "In Function: " << endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = arr[i] * 2;
+//         cout << arr[i] << " ";
+//     }
+// }
+
+int linearSearch(int arr[], int size, int target)
 {
-    cout << "In Function: " << endl;
     for (int i = 0; i < size; i++)
     {
-        arr[i] = arr[i] * 2;
-        cout << arr[i] << " ";
+        if (arr[i] == target)
+        {
+            return i; // Return the index of the target element
+        }
     }
+    return -1; // Return -1 if the target element is not found
 }
 
 int main()
@@ -32,16 +44,33 @@ int main()
 
     // cout << "The smallest element in the array is: " << smallest << " at index " << index << endl;
 
-    // Pass by Reference
+    // Pass by Reference --  it actually passes the address of the array to the function, allowing the function to modify the original array.
 
-    int arr[] = {1, 2, 3};
+    // int arr[] = {1, 2, 3};
 
-    changeArr(arr, 3);
-    cout << endl
-         << "In Main: " << endl;
-    for (int i = 0; i < 3; i++)
+    // changeArr(arr, 3);
+    // cout << endl
+    //      << "In Main: " << endl;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // Linear Search Algorithim
+
+    int arr[] = {2, 4, 7, 8, 5, 3, 1};
+    int size = 7;
+    int target = 5;
+
+    int index = linearSearch(arr, size, target);
+
+    if (index != -1)
     {
-        cout << arr[i] << " ";
+        cout << "Element " << target << " found at index " << index << endl;
+    }
+    else
+    {
+        cout << "Element " << target << " not found in the array." << endl;
     }
 
     return 0;
