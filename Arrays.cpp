09c87ce96e5bc2c8d924b1cbeb34892db6add012
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-31 00:11:45 +0500

    Pass by reference

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,25 +2,47 @@
 #include <climits>
 using namespace std;
 
-int main()
+void changeArr(int arr[], int size)
 {
-    int arr[] = { 2, 3, 4, 5,1};
-    int size = 5;
-    int smallest = INT_MAX; // Initialize smallest to the maximum integer value
-    int index;
+    cout << "In Function: " << endl;
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] < smallest)
-        {
-            smallest = arr[i];
-        }
-        if (arr[i] == smallest)
-        {
-            index = i;
-        }
+        arr[i] = arr[i] * 2;
+        cout << arr[i] << " ";
     }
+}
 
-    cout << "The smallest element in the array is: " << smallest << " at index " << index << endl;
+int main()
+{
+    // int arr[] = { 2, 3, 4, 5,1};
+    // int size = 5;
+    // int smallest = INT_MAX; // Initialize smallest to the maximum integer value
+    // int index;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     if (arr[i] < smallest)
+    //     {
+    //         smallest = arr[i];
+    //     }
+    //     if (arr[i] == smallest)
+    //     {
+    //         index = i;
+    //     }
+    // }
+
+    // cout << "The smallest element in the array is: " << smallest << " at index " << index << endl;
+
+    // Pass by Reference
+
+    int arr[] = {1, 2, 3};
+
+    changeArr(arr, 3);
+    cout << endl
+         << "In Main: " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
