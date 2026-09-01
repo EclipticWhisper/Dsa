fb49a1d7582c653b6ccf6e4a9b6e3250d4a50f62
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-09-01 22:33:06 +0500

    peoduct and sume

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,72 +1,24 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 
-// void changeArr(int arr[], int size)
-// {
-//     cout << "In Function: " << endl;
-//     for (int i = 0; i < size; i++)
-//     {
-//         arr[i] = arr[i] * 2;
-//         cout << arr[i] << " ";
-//     }
-// }
-
-int reverseArray(int arr[], int size)
+int product_add(int arr[], int size)
 {
-    int start = 0;
-    int end = size - 1;
-    while (start < end)
+    int product = 1;
+    int sum = 0;
+
+    for (int i = 0; i < size; i++)
     {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
+        product *= arr[i];
+        sum += arr[i];
     }
+
+    return product + sum;
 }
 
 int main()
 {
-    // int arr[] = { 2, 3, 4, 5,1};
-    // int size = 5;
-    // int smallest = INT_MAX; // Initialize smallest to the maximum integer value
-    // int index;
-    // for (int i = 0; i < size; i++)
-    // {
-    //     if (arr[i] < smallest)
-    //     {
-    //         smallest = arr[i];
-    //     }
-    //     if (arr[i] == smallest)
-    //     {
-    //         index = i;
-    //     }
-    // }
-
-    // cout << "The smallest element in the array is: " << smallest << " at index " << index << endl;
-
-    // Pass by Reference --  it actually passes the address of the array to the function, allowing the function to modify the original array.
-
-    // int arr[] = {1, 2, 3};
-
-    // changeArr(arr, 3);
-    // cout << endl
-    //      << "In Main: " << endl;
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-
-    // Linear Search Algorithim
-
-    int arr[] = {2, 4, 7, 8, 5, 3};
-    int size = 6;
-
-    reverseArray(arr, size);
-
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
-    }
-
+    int arr[] = {1, 2, 3};
+    int size = 3;
+    cout << product_add(arr, size) << endl;
     return 0;
 }
